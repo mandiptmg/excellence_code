b0958df98b0fd40c1202dc8dc8dc480984bf2b00
Mandip Tamang <mandiptamang125@gmail.com>
2025-08-28 16:26:08 +0400

    `Added slug property to services array and updated link to include slug in URL`

--- js/services.js
+++ js/services.js
@@ -2,26 +2,31 @@ export default function initServices() {
   const services = [
     {
       title: "Website Design and Development",
+      slug: "website-design-and-development",
       paragraph:
         "We specialize in designing and developing responsive, user-friendly websites that provide an excellent user experience. Our websites are built with the latest technologies to ensure speed, security, and scalability.",
     },
     {
       title: "Digital Marketing and Social Media Management",
+      slug: "digital-marketing",
       paragraph:
         "Our digital marketing services include SEO, content marketing, social media management, and PPC advertising, designed to increase brand visibility, drive traffic, and generate leads in a competitive marketplace.",
     },
     {
       title: "Events and Conferences Organizing",
+      slug: "events-organising",
       paragraph:
         "We provide comprehensive event planning and exhibition services, including venue selection, logistics, on-site management, and custom exhibition stand design to create impactful in-person experiences.",
     },
     {
       title: "Analytics and Performance Optimization",
+      slug: "analytics",
       paragraph:
         "Our analytics service transforms raw data into actionable insights through advanced tools and predictive analytics, helping optimize business operations and improve decision-making.",
     },
     {
       title: "Email & Hosting Services",
+      slug: "email-hosting",
       paragraph:
         "We offer reliable website and email hosting solutions with high uptime, fast speeds, and strong security, along with scalable options to grow with your business.",
     },
@@ -56,7 +61,7 @@ export default function initServices() {
             <div class="vas-service-content">
               <p class="vas-service-paragraph">${service.paragraph}</p>
               <div class="vas-link-wrap">
-                <a href="#" class="read-more-btn">Learn More</a>
+                <a href="/services/${service.slug}.html" class="read-more-btn">Learn More</a>
                 <span class="icon-wrapper">
                   <img src="./assets/arrow45.png" alt="Arrow Icon" />
                 </span>
